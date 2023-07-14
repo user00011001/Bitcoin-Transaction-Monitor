9c4ab10bc90a55b744c9a843ed1b85b849d82a82
net1ife <127271713+net1ife@users.noreply.github.com>
2023-07-14 17:35:12 -0500

    Update btc_transaction_monitor.py

--- btc_transaction_monitor.py
+++ btc_transaction_monitor.py
@@ -1,61 +1,53 @@
 import json
 import datetime
-from websocket._app import WebSocketApp
+from websocket import WebSocketApp
 
+class BitcoinTransactionListener:
+    def __init__(self, url="wss://ws.blockchain.info/inv"):
+        self.ws = WebSocketApp(url, 
+                               on_message=self.on_message, 
+                               on_error=self.on_error, 
+                               on_close=self.on_close)
 
-def on_message(ws, message):
-    """
-    Function called when a message is received. The message contains information about a bitcoin transaction.
-    """
-    msg_json = json.loads(message)
-    print("\nNew Bitcoin Transaction Received:")
-    print("------------------------------")
+        self.ws.on_open = self.on_open
 
-    print("Transaction ID: ", msg_json.get('x', {}).get('hash'))
+    def on_message(self, ws, message):
+        msg_json = json.loads(message)
+        print("\nNew Bitcoin Transaction Received:")
+        print("------------------------------")
 
-    timestamp = msg_json.get('x', {}).get('time')
-    print("Received at: ", datetime.datetime.fromtimestamp(
-        int(timestamp)).strftime('%Y-%m-%d %H:%M:%S'))
+        print("Transaction ID: ", msg_json.get('x', {}).get('hash'))
 
-    print("\nInputs:")
-    for inp in msg_json.get('x', {}).get('inputs', []):
-        print(" - From Address: ", inp.get('prev_out', {}).get('addr'))
-        print(" - Amount: ", inp.get('prev_out', {}).get('value', 0) / 1e8, " BTC")
+        timestamp = msg_json.get('x', {}).get('time')
+        print("Received at: ", datetime.datetime.fromtimestamp(
+            int(timestamp)).strftime('%Y-%m-%d %H:%M:%S'))
 
-    print("\nOutputs:")
-    for out in msg_json.get('x', {}).get('out', []):
-        print(" - To Address: ", out.get('addr'))
-        print(" - Amount: ", out.get('value', 0) / 1e8, " BTC")
+        print("\nInputs:")
+        for inp in msg_json.get('x', {}).get('inputs', []):
+            print(" - From Address: ", inp.get('prev_out', {}).get('addr'))
+            print(" - Amount: ", inp.get('prev_out', {}).get('value', 0) / 1e8, " BTC")
 
-    print("------------------------------")
+        print("\nOutputs:")
+        for out in msg_json.get('x', {}).get('out', []):
+            print(" - To Address: ", out.get('addr'))
+            print(" - Amount: ", out.get('value', 0) / 1e8, " BTC")
 
+        print("------------------------------")
 
-def on_error(ws, error):
-    """
-    Function called when an error occurs.
-    """
-    print("Error occurred: ", error)
+    def on_error(self, ws, error):
+        print("Error occurred: ", error)
 
+    def on_close(self, ws):
+        print("\nConnection closed")
 
-def on_close(ws):
-    """
-    Function called when the WebSocket connection is closed.
-    """
-    print("\nConnection closed")
+    def on_open(self, ws):
+        ws.send(json.dumps({"op": "unconfirmed_sub"}))
 
-
-def on_open(ws):
-    """
-    Function called when the WebSocket connection is opened.
-    """
-    # Subscribe to unconfirmed transactions
-    ws.send(json.dumps({"op": "unconfirmed_sub"}))
+    def run(self):
+        self.ws.run_forever()
 
 
 if __name__ == "__main__":
-    ws = WebSocketApp("wss://ws.blockchain.info/inv",
-                      on_message=on_message,
-                      on_error=on_error,
-                      on_close=on_close)
-    ws.on_open = on_open
-    ws.run_forever()
+    listener = BitcoinTransactionListener()
+    listener.run()
+
